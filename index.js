92453edc76c8f9386728a10c47832ca7e9f986ab
PJ Frias <peejfrias@gmail.com>
2017-11-08 03:52:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ const fareTripler = function(fare) {
   return createFareMultiplier(3)(fare)
 }
 
-//function selectingDrivers(array,driverSelector) {
-  //return driverSelector(array)()
-//}
+function selectDifferentDrivers(array,driverSelector) {
+  return driverSelector(array)()
+}
