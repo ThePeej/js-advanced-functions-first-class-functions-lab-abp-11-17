154ca73b073c5c274b5924161a4a2f2677926a92
PJ Frias <peejfrias@gmail.com>
2017-11-08 03:37:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const returnFirstTwoDrivers = function(array) {
   let names = array.slice(0,2);
   return names;
 }
+
+const returnLastTwoDrivers = function(array) {
+  let names = array.slice(array.length-2);
+  return names;
+}
